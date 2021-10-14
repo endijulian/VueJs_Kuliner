e668dfeaba9b1e5b527419877585816f27878c44
endi julian <endi@iglobal.co.id>
2021-10-15 03:55:44 +0700

    Navbar

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+  },
+};
+</script>
